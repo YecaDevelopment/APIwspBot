41a863faca2a4f67dda8d925d0f7a8d127f5f29c
unknown <julian.f.vega@gmail.com>
2024-10-12 15:18:14 -0300

    setting webhook co & s

--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
 
 @Injectable()
-export class WebhookService {}
+export class WebhookService {
+
+    constructor(private readonly configServ : ConfigService){}
+
+    async sendMessage(to : string){
+        try {
+            const URL : string = `${this.configServ.get('WSP_URI')}${this.configServ.get('WSP_PHONENUM')}/messages`;
+            const data : {} = {
+                messaging_product: 'whatsapp',
+                to: to,
+                type: 'template',
+                template: {
+                    name: 'hello_world',
+                    language: {
+                        code: 'en_US',
+                    },
+                },
+            };
+
+            const response = await axios.post(URL, data, {
+                headers: {
+                    Authorization: `Bearer ${this.configServ.get('WSP_ACCTOKEN')}`,
+                    "Content-Type": 'application/json'
+                },
+            })
+            return response
+        } 
+        catch (error) { return {status: 'FAIL webhookSERV', error}}
+    }
+}
