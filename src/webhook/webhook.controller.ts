1942339cddfd471549641fc7db6670ce1fb979f7
unknown <julian.f.vega@gmail.com>
2024-10-12 16:49:44 -0300

    setting serv in controller

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -33,7 +33,7 @@ export class WebhookController {
                 const newFrom = '54' + from.slice(3, from.length);
                 console.log('from USER: ', newFrom);
                 
-                const data = await this.webHookServ.sendMessage(from);
+                const data = await this.webHookServ.sendMessage(newFrom);
                 console.log('data: ', data);
                 
             }
