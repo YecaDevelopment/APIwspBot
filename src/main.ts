91bd79b6d928e14562243f2ed03b28ced9b7c393
unknown <julian.f.vega@gmail.com>
2024-10-14 19:02:59 -0300

    debugg deployVercel -> HBS

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import * as hbs from 'hbs';
+import * as hbs from 'express-handlebars';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.engine(hbs)
+  app.engine('hbs', 
+    hbs.engine({
+      extname: 'hbs',
+      defaultLayout: 'main',
+      layoutsDir: join(__dirname, '..', 'views', 'layouts'),
+      partialsDir: join(__dirname, '..', 'views', 'partials')
+    })
+  )
   app.setViewEngine('hbs')
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
 
   await app.listen(3000);
 }
