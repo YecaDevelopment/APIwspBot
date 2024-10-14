fb173d8c44f2e80ec00a6d21f55c18189f9d6f57
unknown <julian.f.vega@gmail.com>
2024-10-14 18:32:28 -0300

    ADD T&C, promise of POP

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('T&C')
+  @Render('t&c')
+  getTYC() {
+    try {return {message: 'Showing T&C'}} 
+    catch (error) {return error}
+  }
+
+  @Get('POP')
+  @Render('pop')
+  getPOP() {
+    try {return {message: 'Showing POP'}} 
+    catch (error) {return error}
+  }
 
   @Get()
   async getHello() {
@@ -13,3 +27,4 @@ export class AppController {
     catch (error) { return error }    
   }
 }
+
